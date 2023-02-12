1937917d8426ea722c90f97d838f60009c87610f
shaharsol <shahar@gnift.com>
2023-02-12 21:14:46 +0200

    initial

---
+++ cartlassi/includes/class-cartlassi-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Cartlassi
+ * @subpackage Cartlassi/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Cartlassi
+ * @subpackage Cartlassi/includes
+ * @author     Your Name <email@example.com>
+ */
+class Cartlassi_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
