55bbc6459090dafdeddba64dca18fe60620b5323
shaharsol <shahar@gnift.com>
2023-04-11 14:16:29 +0300

    move heavy lifting of is display widghet to the ajax part

--- cartlassi/public/class-cartlassi-widget.php
+++ cartlassi/public/class-cartlassi-widget.php
@@ -25,81 +25,83 @@ class Cartlassi_Widget extends WP_Widget {
 		if (wp_doing_ajax()) {
 			check_ajax_referer(Cartlassi_Constants::NONCE_PUBLIC_NAME, 'nonce');
 
-			extract( $args );
-
-			// $title = apply_filters( 'widget_title', $instance['title'] );
-			$title = 'We think you may like...';
-			
-			$apiKey = get_option(Cartlassi_Constants::API_OPTIONS_NAME)[Cartlassi_Constants::API_KEY_FIELD_NAME];
-	
-			$args = array(
-				'headers'     => array(
-					'Authorization' => "token {$apiKey}"
-				),
-			);
-			$cartId = $this->utils->generate_cart_id();
-			$response = wp_remote_get( "{$this->config->get('api_url')}/carts/{$cartId}/shop", $args );
-	
-			if ( is_wp_error( $response ) ) {
-				$error_message = $response->get_error_message();
-				echo "Something went wrong: $error_message";
-				error_log($error_message);
-				return;
-			}
-	
-			$body = wp_remote_retrieve_body( $response );
-			$data = json_decode( $body );
-			$products = array();
-			$cartItemToProductMap = array();
-			foreach($data as $product) {
-				$the_query = new WP_Query( array( 's' => $product->description ) );
-				if ( $the_query->have_posts() ) {
-					$the_query->the_post();
-					$postID = get_the_ID();
-					array_push($products, $postID);
-					$cartItemToProductMap += [$product->id => $postID];
-				}
-				/* Restore original Post Data */
-				wp_reset_postdata();
-	
-				// limit the items in widget (need to find the default # of items per line from woo config)
-				if ( count($products) == wc_get_theme_support( 'product_blocks::default_columns', 3 )) {
-					break;
-				}
-			}
-	
-			if (count($products) == 0) {
-				return; // TBD replace to wp_die() here?
-			}
-			WC()->session->set(Cartlassi_Constants::CURRENT_MAP_NAME, $cartItemToProductMap);
-			$block_name = 'woocommerce/handpicked-products';
-			$converted_block = new WP_Block_Parser_Block( $block_name, array(
-				'products' => $products,
-				'title'		=> 'We think you may like...'
-			), array(), '', array() );
-	
-			$rendered_block = render_block( (array) $converted_block );
-			
-			echo $rendered_block;
-			wp_die();
-		} else {
-			
 			$paymentMethod = $this->utils->get_payment_method();
 			$isPaymentMethod = $paymentMethod->brand && $paymentMethod->last4;
 			$isAppearanceSet = !!get_option( Cartlassi_Constants::APPEARANCE_OPTIONS_NAME );
 			$isDisplayingWidget = $isAppearanceSet && $isPaymentMethod;
+
 			if ( $isDisplayingWidget ) {
 				extract( $args );
-				// echo $before_widget;
-				echo '<div id="cartlassi-widget-container"><span id="cartlassi-widget-title">We think you may like</span>';
-				if ( ! empty( $title ) ) {
-					//echo $before_title . $title . $after_title;
+
+				// $title = apply_filters( 'widget_title', $instance['title'] );
+				$title = 'We think you may like...';
+				
+				$apiKey = get_option(Cartlassi_Constants::API_OPTIONS_NAME)[Cartlassi_Constants::API_KEY_FIELD_NAME];
+		
+				$args = array(
+					'headers'     => array(
+						'Authorization' => "token {$apiKey}"
+					),
+				);
+				$cartId = $this->utils->generate_cart_id();
+				$response = wp_remote_get( "{$this->config->get('api_url')}/carts/{$cartId}/shop", $args );
+		
+				if ( is_wp_error( $response ) ) {
+					$error_message = $response->get_error_message();
+					echo "Something went wrong: $error_message";
+					error_log($error_message);
+					return;
 				}
-				echo '<div id="cartlassi-ajax-widget"></div>';
-				echo '<div id="powered-by-cartlassi">powered by <a href="https://cartlassi.com">Cartlassi</a></div>';
-				echo '</div>';
-				// echo $after_widget;
+		
+				$body = wp_remote_retrieve_body( $response );
+				$data = json_decode( $body );
+				$products = array();
+				$cartItemToProductMap = array();
+				foreach($data as $product) {
+					$the_query = new WP_Query( array( 's' => $product->description ) );
+					if ( $the_query->have_posts() ) {
+						$the_query->the_post();
+						$postID = get_the_ID();
+						array_push($products, $postID);
+						$cartItemToProductMap += [$product->id => $postID];
+					}
+					/* Restore original Post Data */
+					wp_reset_postdata();
+		
+					// limit the items in widget (need to find the default # of items per line from woo config)
+					if ( count($products) == wc_get_theme_support( 'product_blocks::default_columns', 3 )) {
+						break;
+					}
+				}
+		
+				if (count($products) == 0) {
+					return; // TBD replace to wp_die() here?
+				}
+				WC()->session->set(Cartlassi_Constants::CURRENT_MAP_NAME, $cartItemToProductMap);
+				$block_name = 'woocommerce/handpicked-products';
+				$converted_block = new WP_Block_Parser_Block( $block_name, array(
+					'products' => $products,
+					'title'		=> 'We think you may like...'
+				), array(), '', array() );
+		
+				$rendered_block = render_block( (array) $converted_block );
+				
+				echo $rendered_block;
 			}
+
+			wp_die();
+		} else {
+			
+			extract( $args );
+			// echo $before_widget;
+			echo '<div id="cartlassi-widget-container"><span id="cartlassi-widget-title">We think you may like</span>';
+			if ( ! empty( $title ) ) {
+				//echo $before_title . $title . $after_title;
+			}
+			echo '<div id="cartlassi-ajax-widget"></div>';
+			echo '<div id="powered-by-cartlassi">powered by <a href="https://cartlassi.com">Cartlassi</a></div>';
+			echo '</div>';
+			// echo $after_widget;
 		}
 	}
 
