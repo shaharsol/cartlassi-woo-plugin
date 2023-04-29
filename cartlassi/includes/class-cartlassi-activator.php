3a185e2f45c3acb72d731327f9ae2f211f967a10
shaharsol <shahar@gnift.com>
2023-04-29 14:42:55 +0300

    activate may need to work if not first install

--- cartlassi/includes/class-cartlassi-activator.php
+++ cartlassi/includes/class-cartlassi-activator.php
@@ -30,23 +30,27 @@ class Cartlassi_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-		// $config = new Cartlassi_Config();
-		// $api = new Cartlassi_Api($config);
+		// notify the server the store is active again
+		// otherwise if it's a 1st install leave the signup for admin
+		if (get_option(Cartlassi_Constants::API_OPTIONS_NAME)) {
+			$config = new Cartlassi_Config();
+			$api = new Cartlassi_Api($config);
 
-		// $body = array(
-		// 	'url'  	=> get_bloginfo('url'),
-		// 	'email' => get_bloginfo('admin_email'),
-		// 	'name'  => get_bloginfo('name'),
-		// 	'country' => wc_get_base_location()['country']
-		// );
-		// $args = array(
-		// 	'method'	=> 'POST',
-		// 	'body'        => $body,
-		// );
-		// $data = $api->request( "/shops/register", $args );
-		// update_option (Cartlassi_Constants::API_OPTIONS_NAME, array (
-		// 	Cartlassi_Constants::API_KEY_FIELD_NAME => $data->apiKey,
-		// 	Cartlassi_Constants::API_SECRET_FIELD_NAME => $data->apiSecret,
-		// ));
+			$body = array(
+				'url'  	=> get_bloginfo('url'),
+				'email' => get_bloginfo('admin_email'),
+				'name'  => get_bloginfo('name'),
+				'country' => wc_get_base_location()['country']
+			);
+			$args = array(
+				'method'	=> 'POST',
+				'body'        => $body,
+			);
+			$data = $api->request( "/shops/register", $args );
+			update_option (Cartlassi_Constants::API_OPTIONS_NAME, array (
+				Cartlassi_Constants::API_KEY_FIELD_NAME => $data->apiKey,
+				Cartlassi_Constants::API_SECRET_FIELD_NAME => $data->apiSecret,
+			));
+		}
 	}
 }
